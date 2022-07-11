b466813b8a7ea1cc0fe0c897c7d0a74f6c326f1f
arlestt <jangatiloknath2002@gmail.com>
2022-07-11 15:37:54 +0530

    tried to understand the logic for todolist

---
+++ TodoListProject/todolist.js
@@ -0,0 +1,13 @@
+console.log("hello")
+c = 1;
+// let input = prompt("what do you want to do?");
+
+// while(input == "new" || input == "list" || input == "delete" || input == "quit"){
+//     if(input == "quit"){
+//         break;
+//     }
+//     console.log(c);
+//     c++;
+// }
+
+console.log("you are out of the loop");
