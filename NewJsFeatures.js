2a2ff729392eac63943b43d16d31766aee325d11
arlestt <jangatiloknath2002@gmail.com>
2022-07-18 15:11:56 +0530

    new features

---
+++ NewJsFeatures.js
@@ -0,0 +1,12 @@
+console.log("new js features!");
+
+
+//default parameters
+const multiply = (a,b=1) => a*b; //here even if b is not defined, the compiler takes the default value that is b=1.
+console.log(multiply(7,2));
+console.log(multiply(7));
+
+function rollDie (numSides = 6){
+    return Math.floor(Math.random()*numSides+1);
+}  
+console.log(rollDie());
--- app.js
+++
@@ -1,3 +0,0 @@
-console.log("hello git");
-console.log("hello git 2");
-console.log("hello git!");
